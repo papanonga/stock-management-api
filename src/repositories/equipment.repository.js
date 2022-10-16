2f4f5ffdb50dff4b19ab748fa162976efc713544
tamagod <kimjonggod@hotmail.com>
2022-10-16 20:52:27 +0700

    model name configuration (rename History.js to history.js)

--- src/repositories/equipment.repository.js
+++ src/repositories/equipment.repository.js
@@ -1,30 +1,30 @@
 const db = require('../db/models')
 
 
-exports.findAllEquipment = async () => await db.Equipments.findAll({
+exports.findAllEquipment = async () => await db.equipments.findAll({
     order: [
         ['id', 'ASC']
     ]
 })
 
-exports.findByID = async id => await db.Equipments.findByPk(id)
+exports.findByID = async id => await db.equipments.findByPk(id)
 
-exports.addEquipment = async equipment => await db.Equipments.create(equipment)
+exports.addEquipment = async equipment => await db.equipments.create(equipment)
 
-exports.updateEquipmentDetail = async (id, equipment) => await db.Equipments.update(equipment, {
+exports.updateEquipmentDetail = async (id, equipment) => await db.equipments.update(equipment, {
     where: {
         id
     }
 })
 
-exports.deleteEquipment = async id => await db.Equipments.destroy({
+exports.deleteEquipment = async id => await db.equipments.destroy({
     where: {
         id
     }
 })
 
 
-exports.findBySerialNumber = async sn => await db.Equipments.findAll({
+exports.findBySerialNumber = async sn => await db.equipments.findAll({
     where: {
         serial_number: sn
     }
--- src/repositories/history.repository.js
+++ src/repositories/history.repository.js
@@ -1,20 +1,20 @@
 const historyDatabase = require('../db/models')
 
-exports.getAllTransaction = async () => await historyDatabase.History.findAll()
+exports.getAllTransaction = async () => await historyDatabase.history.findAll()
 
 
-exports.getBySerialNumber = async equipment_sn => await historyDatabase.History.findAll({
+exports.getBySerialNumber = async equipment_sn => await historyDatabase.history.findAll({
     where : {
         equipment_sn
     }
 })
 
 
-exports.getByProject = async project_name => await historyDatabase.History.findAll({
+exports.getByProject = async project_name => await historyDatabase.history.findAll({
     where : {
         project_name
     }
 })
 
 
-exports.writeHistory = async data => await historyDatabase.History.create(data)
+exports.writehistory = async data => await historyDatabase.history.create(data)
